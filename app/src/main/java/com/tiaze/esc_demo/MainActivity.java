cd379816d1393807a138f234bfac1c8f7f1a32af
BuYishi <wolf-pan@qq.com>
2018-09-26 10:27:23 +0800

    first commit

---
+++ app/src/main/java/com/tiaze/esc_demo/MainActivity.java
@@ -0,0 +1,171 @@
+package com.tiaze.esc_demo;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private PrintTool printTool;
+    private final String tag = MainActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.enableBluetoothButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (bluetoothAdapter.isEnabled()) {
+                    Toast.makeText(MainActivity.this, "蓝牙已启用", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//                    startActivityForResult(intent, 0);
+                    startActivity(intent);
+                }
+            }
+        });
+        findViewById(R.id.printTextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (printTool != null) {
+                    try {
+                        printTool.printText("天择信上科技\n", 1, 2, PrintTool.ALIGNMENT_CENTER);
+                        printTool.printText("天择信上科技\n", 1, 1, PrintTool.ALIGNMENT_CENTER);
+                        StringBuilder contentsToPrint = new StringBuilder();
+                        contentsToPrint.append("支付方式：微信支付(预授权)\n");
+                        contentsToPrint.append("订单类型：押金结算后退款\n");
+                        contentsToPrint.append("交易时间：2018-09-25 11:41:00\n");
+                        contentsToPrint.append("订单号：tk20180925114100\n");
+                        contentsToPrint.append("流水号：\n00000000000000000000\n");
+                        contentsToPrint.append("-----------------------------\n");
+                        printTool.printText(contentsToPrint.toString(), 1, 1, PrintTool.ALIGNMENT_LEFT);
+                        printTool.printText("业务记录：\n", 1, 2, PrintTool.ALIGNMENT_LEFT);
+                        contentsToPrint = new StringBuilder();
+                        contentsToPrint.append("押金：￥0.01(09-25 11:41:00)\n");
+                        contentsToPrint.append("消费：￥0.01\n");
+                        contentsToPrint.append("结算退款：￥0.01(09-25 11:41:00)\n");
+                        contentsToPrint.append("退款：￥0.01(09-25 11:41:00)\n");
+                        contentsToPrint.append("-----------------------------\n");
+                        printTool.printText(contentsToPrint.toString(), 1, 1, PrintTool.ALIGNMENT_LEFT);
+                        printTool.printText("退款：￥0.01\n", 1, 2, PrintTool.ALIGNMENT_LEFT);
+                        printTool.printText("备注：打印模板测试\n", 1, 1, PrintTool.ALIGNMENT_LEFT);
+                    } catch (IOException ex) {
+                        Log.e(tag, "ex: " + ex);
+                    }
+                }
+            }
+        });
+        findViewById(R.id.printBarcodeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BlueToothUtils blueToothUtils = BlueToothUtils.getInstance();
+                List<BluetoothDevice> bondedDevices = blueToothUtils.getBondedDevices();
+                Log.d("Debug", "bondedDevices: " + bondedDevices);
+//                Debug: bondedDevices: [00:11:22:33:44:55]
+
+                String barcodeData = "1234567890";
+                byte[] arrayOfByte = new byte[13 + barcodeData.length()];
+                // 设置条码高度
+                arrayOfByte[0] = 0x1D;
+                arrayOfByte[1] = 'h';
+                arrayOfByte[2] = 0x60; // 1到255
+
+                // 设置条码宽度
+                arrayOfByte[3] = 0x1D;
+                arrayOfByte[4] = 'w';
+                arrayOfByte[5] = 2; // 2到6
+
+                // 设置条码文字打印位置
+                arrayOfByte[6] = 0x1D;
+                arrayOfByte[7] = 'H';
+                arrayOfByte[8] = 2; // 0到3
+
+                // 打印39条码
+                arrayOfByte[9] = 0x1D;
+                arrayOfByte[10] = 'k';
+                arrayOfByte[11] = 0x45;
+                arrayOfByte[12] = ((byte) barcodeData.length());
+                System.arraycopy(barcodeData.getBytes(), 0, arrayOfByte, 13,
+                        barcodeData.getBytes().length);
+
+                try {
+                    blueToothUtils.connectDevice(blueToothUtils.getBondedDevices().get(0));
+                    boolean returnValue = blueToothUtils.sendData(arrayOfByte.length, arrayOfByte);
+                    Log.d("Debug", "returnValue: " + returnValue);
+                    String newline = "\n";
+                    byte[] data = newline.getBytes();
+                    returnValue = blueToothUtils.sendData(data.length, data);
+                    Log.d("Debug", "returnValue: " + returnValue);
+                } catch (IOException ex) {
+                    Log.e("Error", "ex: " + ex);
+                }
+            }
+        });
+        findViewById(R.id.printQRCodeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(tag, "Build.MODEL: " + Build.MODEL);
+                if (printTool != null) {
+                    try {
+//                        printTool.printText("\n\n\n\n", 1, 1, PrintTool.ALIGNMENT_LEFT);
+                        printTool.printQRCode("All is well!\n一切安好！", 5, PrintTool.ALIGNMENT_CENTER);
+                        printTool.printText("\n\n\n\n", 1, 1, PrintTool.ALIGNMENT_RIGHT);
+                    } catch (IOException ex) {
+                        Log.e(tag, "ex: " + ex);
+                    }
+                }
+            }
+        });
+        findViewById(R.id.printBitmapButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (printTool != null) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getAssets().open("ic_launcher.png"));
+                        printTool.printBitmap(bitmap, PrintTool.ALIGNMENT_RIGHT);
+                        printTool.printText("\n\n\n\n", 1, 1, PrintTool.ALIGNMENT_CENTER);
+                    } catch (IOException ex) {
+                        Log.e(tag, "ex: " + ex);
+                    }
+                }
+            }
+        });
+        findViewById(R.id.printTemplateButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (printTool != null) {
+                    try {
+                        printTool.printTemplate(MainActivity.this);
+                    } catch (IOException ex) {
+                        Log.e(tag, "ex: " + ex);
+                    }
+                }
+            }
+        });
+        BlueToothUtils blueToothUtils = BlueToothUtils.getInstance();
+        List<BluetoothDevice> bondedDevices = blueToothUtils.getBondedDevices();
+        if (bondedDevices.size() == 0) {
+            Toast.makeText(this, "未配对蓝牙打印机", Toast.LENGTH_SHORT).show();
+        } else {
+            try {
+                blueToothUtils.connectDevice(bondedDevices.get(0));
+                printTool = new PrintTool(blueToothUtils);
+            } catch (IOException ex) {
+                Log.e(tag, "ex: " + ex);
+                Toast.makeText(this, "未能连接蓝牙设备", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/tiaze/esc_demo/PrintTool.java
@@ -0,0 +1,240 @@
+package com.tiaze.esc_demo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class PrintTool {
+    private BlueToothUtils blueToothUtils;
+    private final String tag = "PrintTool";
+    public static final byte ALIGNMENT_LEFT = 48;
+    public static final byte ALIGNMENT_CENTER = 49;
+    public static final byte ALIGNMENT_RIGHT = 50;
+
+    public PrintTool(BlueToothUtils blueToothUtils) {
+        this.blueToothUtils = blueToothUtils;
+    }
+
+    //fontSize范围：1-8
+    public void printText(String text, int fontWidth, int fontHeight, byte alignment) throws IOException {
+        setAlignment(alignment);
+        byte fontWidthByte, fontHeightByte;
+        switch (fontWidth) {
+            case 1:
+                fontWidthByte = 0x00;
+                break;
+            case 2:
+                fontWidthByte = 0x10;
+                break;
+            case 3:
+                fontWidthByte = 0x20;
+                break;
+            case 4:
+                fontWidthByte = 0x30;
+                break;
+            case 5:
+                fontWidthByte = 0x40;
+                break;
+            case 6:
+                fontWidthByte = 0x50;
+                break;
+            case 7:
+                fontWidthByte = 0x60;
+                break;
+            default:
+                fontWidthByte = 0x70;
+        }
+        switch (fontHeight) {
+            case 1:
+                fontHeightByte = 0x00;
+                break;
+            case 2:
+                fontHeightByte = 0x01;
+                break;
+            case 3:
+                fontHeightByte = 0x02;
+                break;
+            case 4:
+                fontHeightByte = 0x03;
+                break;
+            case 5:
+                fontHeightByte = 0x04;
+                break;
+            case 6:
+                fontHeightByte = 0x05;
+                break;
+            case 7:
+                fontHeightByte = 0x06;
+                break;
+            default:
+                fontHeightByte = 0x07;
+        }
+        byte[] bytesForFontSize = {29, 33, (byte) (fontWidthByte | fontHeightByte)};
+        byte[] textBytes = text.getBytes("GBK");
+        byte[] buffer = new byte[bytesForFontSize.length + textBytes.length];
+        System.arraycopy(bytesForFontSize, 0, buffer, 0, bytesForFontSize.length);
+        System.arraycopy(textBytes, 0, buffer, bytesForFontSize.length, textBytes.length);
+        blueToothUtils.sendData(buffer.length, buffer);
+    }
+
+    //unitSize范围：1-16
+    public void printQRCode(String data, int unitSize, byte alignment) throws IOException {  //商睿通过指令打印二维码无法居右
+        if (Build.MODEL.contains("A8") || Build.MODEL.contains("JICAI"))  //A8与集财打印二维码，须转换为图片后打印
+            printQRCodeBitmap(data, unitSize * 30, alignment);
+        else {
+            setAlignment(alignment);
+            byte[] bytesForQRCodeUnitSize = {29, 40, 107, 3, 0, 49, 67, (byte) unitSize};
+            byte[] bytesForStoreQRCodeData = new byte[]{29, 40, 107, 0, 0, 49, 80, 48};
+            byte[] qrcodeDataBytes = data.getBytes();
+            int cmdDataLen = qrcodeDataBytes.length + 3;
+            bytesForStoreQRCodeData[3] = (byte) (cmdDataLen % 256);
+            bytesForStoreQRCodeData[4] = (byte) (cmdDataLen / 256);
+            byte[] bytesForPrintQRCode = {29, 40, 107, 3, 0, 49, 81, 48};
+            byte[] buffer = new byte[bytesForQRCodeUnitSize.length + bytesForStoreQRCodeData.length + qrcodeDataBytes.length + bytesForPrintQRCode.length];
+            System.arraycopy(bytesForQRCodeUnitSize, 0, buffer, 0, bytesForQRCodeUnitSize.length);
+            System.arraycopy(bytesForStoreQRCodeData, 0, buffer, bytesForQRCodeUnitSize.length, bytesForStoreQRCodeData.length);
+            System.arraycopy(qrcodeDataBytes, 0, buffer, bytesForQRCodeUnitSize.length + bytesForStoreQRCodeData.length, qrcodeDataBytes.length);
+            System.arraycopy(bytesForPrintQRCode, 0, buffer, bytesForQRCodeUnitSize.length + bytesForStoreQRCodeData.length + qrcodeDataBytes.length, bytesForPrintQRCode.length);
+            blueToothUtils.sendData(buffer.length, buffer);
+            if (Build.MODEL.contains("P1") || Build.MODEL.contains("V1s"))
+                printText("\n", 1, 1, alignment);
+        }
+    }
+
+    public void printBitmap(Bitmap bitmap, byte alignment) throws IOException {
+        setAlignment(alignment);
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int pixelRed;
+        int pixelGreen;
+        int pixelBlue;
+        int[] pixels = new int[width * height];
+        int arrayWidth = ((width - 1) / 8) + 1;
+        int arrayLength = arrayWidth * height;
+        byte[] dataArray = new byte[arrayLength + 8];
+        dataArray[0] = 0x1D;
+        dataArray[1] = 0x76;
+        dataArray[2] = 0x30;
+        dataArray[3] = 0x00;
+        dataArray[4] = (byte) arrayWidth;
+        dataArray[5] = (byte) (arrayWidth / 256);
+        dataArray[6] = (byte) height;
+        dataArray[7] = (byte) (height / 256);
+        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+        int indexByte = 8;
+        dataArray[indexByte] = 0;
+        int indexBit = 0;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {//每一行进行转换，转换完成后，可能最后一个字节需要将数据移到高位
+                //获取当前像素值的r部分
+                pixelRed = Color.red(pixels[i * width + j]);
+                //获取当前像素值的g部分
+                pixelGreen = Color.green(pixels[i * width + j]);
+                //获取当前像素值的b部分
+                pixelBlue = Color.blue(pixels[i * width + j]);
+                if ((pixelRed + pixelGreen + pixelBlue) < 384) {
+                    dataArray[indexByte] += 0x01;
+                }
+                ++indexBit;
+                if (indexBit < 8) {
+                    dataArray[indexByte] *= 2;//相当于左移一位
+                } else {
+                    indexBit = 0;
+                    ++indexByte;
+                    if (indexByte < arrayLength) {
+                        dataArray[indexByte] = 0;
+                    }
+                }
+            }
+            if (indexBit != 0) {
+                while (indexBit < 8) {
+                    dataArray[indexByte] *= 2;//相当于左移一位
+                    ++indexBit;
+                }
+                indexBit = 0;
+                ++indexByte;
+                if (indexByte < arrayLength) {
+                    dataArray[indexByte] = 0;
+                }
+            }
+        }
+        blueToothUtils.sendData(dataArray.length, dataArray);
+    }
+
+    public void printTemplate(Context context) throws IOException {
+        Bitmap bitmap = BitmapFactory.decodeStream(context.getAssets().open("icon_alipay.bmp"));
+        printBitmap(bitmap, PrintTool.ALIGNMENT_LEFT);
+        printText("天择信上科技\n", 1, 2, PrintTool.ALIGNMENT_CENTER);
+        printText("天择信上科技\n", 1, 1, PrintTool.ALIGNMENT_CENTER);
+        printText("\n", 1, 1, PrintTool.ALIGNMENT_LEFT);
+        if (Build.MODEL.contains("JICAI"))
+            printText("\n", 1, 1, PrintTool.ALIGNMENT_LEFT);
+        StringBuilder contentsToPrint = new StringBuilder();
+        contentsToPrint.append("支付方式：微信支付(预授权)\n");
+        contentsToPrint.append("订单类型：押金结算后退款\n");
+        contentsToPrint.append("交易时间：2018-09-25 11:41:00\n");
+        contentsToPrint.append("订单号：tk20180925114100\n");
+        contentsToPrint.append("流水号：\n00000000000000000000\n");
+        contentsToPrint.append("-----------------------------\n");
+        printText(contentsToPrint.toString(), 1, 1, PrintTool.ALIGNMENT_LEFT);
+        printText("业务记录：\n", 1, 2, PrintTool.ALIGNMENT_LEFT);
+        contentsToPrint = new StringBuilder();
+        contentsToPrint.append("押金：￥0.01(09-25 11:41:00)\n");
+        contentsToPrint.append("消费：￥0.01\n");
+        contentsToPrint.append("结算退款：￥0.01(09-25 11:41:00)\n");
+        contentsToPrint.append("退款：￥0.01(09-25 11:41:00)\n");
+        contentsToPrint.append("-----------------------------\n");
+        printText(contentsToPrint.toString(), 1, 1, PrintTool.ALIGNMENT_LEFT);
+        printText("退款：￥0.01\n", 1, 2, PrintTool.ALIGNMENT_LEFT);
+        printText("备注：打印模板测试\n", 1, 1, PrintTool.ALIGNMENT_LEFT);
+        printText("\n", 1, 1, PrintTool.ALIGNMENT_LEFT);
+        if (Build.MODEL.contains("JICAI"))
+            printText("\n", 1, 1, PrintTool.ALIGNMENT_LEFT);
+        printQRCode("布伊什\nBu Ish", 5, PrintTool.ALIGNMENT_CENTER);
+        printText("\n", 1, 1, PrintTool.ALIGNMENT_LEFT);
+        contentsToPrint = new StringBuilder();
+        contentsToPrint.append("收银员：天择信上科技\n");
+        contentsToPrint.append("打印时间：2018-09-25 11:56:00\n");
+        if (Build.MODEL.contains("A8"))
+            contentsToPrint.append("\n\n\n\n");
+        else
+            contentsToPrint.append("\n\n\n");
+        printText(contentsToPrint.toString(), 1, 1, PrintTool.ALIGNMENT_LEFT);
+    }
+
+    //size范围：1-16
+    private void printQRCodeBitmap(String data, int size, byte alignment) throws IOException {
+        try {
+            HashMap<EncodeHintType, String> hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            hints.put(EncodeHintType.MARGIN, "0");
+            BitMatrix bitMatrix = new QRCodeWriter().encode(data, BarcodeFormat.QR_CODE, size, size, hints);
+            int[] colors = new int[size * size];
+            for (int y = 0; y < size; ++y) {
+                for (int x = 0; x < size; ++x)
+                    colors[size * y + x] = bitMatrix.get(x, y) ? Color.RED : Color.WHITE;
+            }
+            Bitmap bitmap = Bitmap.createBitmap(colors, size, size, Bitmap.Config.RGB_565);
+            printBitmap(bitmap, alignment);
+        } catch (WriterException ex) {
+            Log.e(tag, "ex: " + ex);
+        }
+    }
+
+    private void setAlignment(byte alignment) throws IOException {
+        byte[] bytesForAlignment = {27, 97, alignment};
+        blueToothUtils.sendData(bytesForAlignment.length, bytesForAlignment);
+    }
+}
